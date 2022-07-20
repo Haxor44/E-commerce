5ce1b786698d5414eac0b03e14bea51638d08961
Haxor44 <evolmalek04@gmail.com>
2022-07-20 22:31:34 +0300

    Finally!!!

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Dawa',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
