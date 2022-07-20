373993d44da03ec817e616465a04c89d0b7ef1f1
Haxor44 <evolmalek04@gmail.com>
2022-07-20 21:48:16 +0300

    Deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -140,20 +140,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING':False,
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
 
-SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-}
 
-DJOSER = {
-    'SERIALIZERS':{
-        'user_create':'core.serializers.UserCreateSerializer'
-    }
-}
+
+
+
+
 
